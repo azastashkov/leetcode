aed8860800e7636640b1c580e2236ae694dffd6e
azastashkov <zastashkov@yahoo.com>
2025-01-16 22:41:21 +0300

    0020

---
+++ src/main/java/easy/_0020/Solution.java
@@ -0,0 +1,30 @@
+package easy._0020;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.offerFirst(c);
+            } else {
+                Character t = stack.pollFirst();
+                if (t == null) {
+                    return false;
+                }
+
+                if (c == ')' && t != '(') {
+                    return false;
+                } else if (c == '}' && t != '{') {
+                    return false;
+                } else if (c == ']' && t != '[') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
