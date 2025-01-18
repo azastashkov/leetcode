3c4cb99df4b87ea7f26d9f5cf79d3d7467ee8c06
azastashkov <zastashkov@yahoo.com>
2025-01-18 13:57:55 +0300

    0035

---
+++ src/test/java/easy/_0035/SolutionTest.java
@@ -0,0 +1,18 @@
+package easy._0035;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+
+        assertEquals(2, solution.searchInsert(new int[] { 1, 3, 5, 6 }, 5));
+
+        assertEquals(1, solution.searchInsert(new int[] { 1, 3, 5, 6 }, 2));
+
+        assertEquals(4, solution.searchInsert(new int[] { 1, 3, 5, 6 }, 7));
+    }
+}
