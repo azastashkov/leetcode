85429c1e70f8bb2ea5636d25c10c03416ac9baee
azastashkov <zastashkov@yahoo.com>
2025-01-23 19:54:50 +0300

    0225

--- src/test/java/easy/_0200_0299/_0225/MyStackTest.java
+++ src/test/java/easy/_0200_0299/_0225/MyStackTest.java
@@ -3,6 +3,8 @@ package easy._0200_0299._0225;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class MyStackTest {
     @Test
@@ -16,5 +18,22 @@ public class MyStackTest {
         assertEquals(3, myStack.pop());
         assertEquals(2, myStack.pop());
         assertEquals(1, myStack.pop());
+
+        assertTrue(myStack.empty());
+
+        myStack.push(4);
+        myStack.push(5);
+
+        assertEquals(5, myStack.pop());
+        assertFalse(myStack.empty());
+
+        myStack.push(6);
+
+        assertEquals(6, myStack.top());
+
+        assertEquals(6, myStack.pop());
+        assertEquals(4, myStack.pop());
+
+        assertTrue(myStack.empty());
     }
 }
