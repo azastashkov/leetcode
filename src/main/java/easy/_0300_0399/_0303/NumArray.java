96ee797686a22cadc199ad02c6be42e5bfd3b58b
azastashkov <zastashkov@yahoo.com>
2025-01-25 22:42:48 +0300

    0303

---
+++ src/main/java/easy/_0300_0399/_0303/NumArray.java
@@ -0,0 +1,16 @@
+package easy._0300_0399._0303;
+
+public class NumArray {
+    private int[] prefixSum;
+
+    public NumArray(int[] nums) {
+        prefixSum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return prefixSum[right + 1] - prefixSum[left];
+    }
+}
