5c325f14625e9187daa3c6bfca53894e9cf86900
azastashkov <zastashkov@yahoo.com>
2025-04-13 22:00:35 +0300

    1037

---
+++ src/test/java/easy/_1000_1999/_1037/SolutionTest.java
@@ -0,0 +1,19 @@
+package easy._1000_1999._1037;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+
+        assertTrue(solution.isBoomerang(new int[][] { { 1, 1 }, { 2, 3 }, { 3, 2 } }));
+
+        assertFalse(solution.isBoomerang(new int[][] { { 1, 1 }, { 2, 2 }, { 3, 3 } }));
+
+        assertTrue(solution.isBoomerang(new int[][] { { 0, 0 }, { 1, 0 }, { 2, 2 } }));
+    }
+}
