b10f11cc91c83c4b278b0280402e87d523326f5f
azastashkov <zastashkov@yahoo.com>
2025-04-13 21:19:06 +0300

    1025

---
+++ src/main/java/easy/_1000_1999/_1025/Solution.java
@@ -0,0 +1,7 @@
+package easy._1000_1999._1025;
+
+public class Solution {
+    public boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+}
---
+++ src/test/java/easy/_1000_1999/_1025/SolutionTest.java
@@ -0,0 +1,17 @@
+package easy._1000_1999._1025;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+
+        assertTrue(solution.divisorGame(2));
+
+        assertFalse(solution.divisorGame(3));
+    }
+}
