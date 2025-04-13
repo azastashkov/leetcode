7514134402c7d5a77e903a3325c1b578f0712f1e
azastashkov <zastashkov@yahoo.com>
2025-04-13 19:25:54 +0300

    1021

--- src/main/java/easy/_1000_1999/_1021/Solution.java
+++ src/main/java/easy/_1000_1999/_1021/Solution.java
@@ -1,28 +1,20 @@
 package easy._1000_1999._1021;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 public class Solution {
     public String removeOuterParentheses(String s) {
         int l = s.length();
-        int p = 0;
+        int count = 0;
         StringBuilder sb = new StringBuilder();
-        Deque<Character> stack = new ArrayDeque<>();
         for (int i = 0; i < l; i++) {
             char c = s.charAt(i);
             if (c == '(') {
-                if (stack.isEmpty()) {
-                    p++;
+                if (++count > 1) {
+                    sb.append(c);
                 }
-                stack.push(c);
             } else if (c == ')') {
-                stack.pop();
-            }
-
-            if (stack.isEmpty()) {
-                sb.append(s, p, i);
-                p = i + 1;
+                if (--count > 0) {
+                    sb.append(c);
+                }
             }
         }
 
