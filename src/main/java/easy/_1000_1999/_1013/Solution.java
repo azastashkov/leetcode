4d78d2910975f8cf40d97c74cb86bfc5db301dbf
azastashkov <zastashkov@yahoo.com>
2025-04-13 17:11:44 +0300

    1013

---
+++ src/main/java/easy/_1000_1999/_1013/Solution.java
@@ -0,0 +1,25 @@
+package easy._1000_1999._1013;
+
+public class Solution {
+    public boolean canThreePartsEqualSum(int[] arr) {
+        int total = 0;
+        for (int i : arr) {
+            total += i;
+        }
+
+        if (total % 3 != 0) {
+            return false;
+        }
+
+        int c = 0, s = total / 3, t = 0;
+        for (int i : arr) {
+            t += i;
+            if (t == s) {
+                t = 0;
+                c++;
+            }
+        }
+
+        return c >= 3;
+    }
+}
