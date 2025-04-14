e8ebd5d4ab23647f4bb2c53ac2bd6c24c840b1be
azastashkov <zastashkov@yahoo.com>
2025-04-14 21:15:43 +0300

    1071

---
+++ src/test/java/easy/_1000_1999/_1071/SolutionTest.java
@@ -0,0 +1,18 @@
+package easy._1000_1999._1071;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+
+        assertEquals("ABC", solution.gcdOfStrings("ABCABC", "ABC"));
+
+        assertEquals("AB", solution.gcdOfStrings("ABABAB", "ABAB"));
+
+        assertEquals("", solution.gcdOfStrings("LEET", "CODE"));
+    }
+}
