24e92aa6f1e2119e8b0d47f4f3e9671bc7d0f93a
azastashkov <zastashkov@yahoo.com>
2025-04-18 20:09:45 +0300

    1313

--- src/main/java/easy/_1300_1399/_1313/Solution.java
+++ src/main/java/easy/_1300_1399/_1313/Solution.java
@@ -1,18 +1,17 @@
 package easy._1300_1399._1313;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution {
     public int[] decompressRLElist(int[] nums) {
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list = new LinkedList<>();
         int l = nums.length;
         for (int i = 0; i < l; i += 2) {
             int freq = nums[i];
             int val = nums[i + 1];
-            for (int j = 0; j < freq; j++) {
-                list.add(val);
-            }
+            list.addAll(Collections.nCopies(freq, val));
         }
 
         return list.stream().mapToInt(i -> i).toArray();
