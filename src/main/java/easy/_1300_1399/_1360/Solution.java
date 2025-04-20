29c9c2eba249ceaec37c67cc7b1ad0f7ec986a81
azastashkov <zastashkov@yahoo.com>
2025-04-20 20:07:36 +0300

    1360

--- src/main/java/easy/_1300_1399/_1360/Solution.java
+++ src/main/java/easy/_1300_1399/_1360/Solution.java
@@ -2,48 +2,35 @@ package easy._1300_1399._1360;
 
 public class Solution {
     public int daysBetweenDates(String date1, String date2) {
-        final int[] daysInMonths = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-        int year1 = year(date1);
-        int year2 = year(date2);
-
-        int month1 = month(date1);
-        int month2 = month(date2);
-
-        int day1 = day(date1);
-        int day2 = day(date2);
-
-        return Math.abs(calcDays(year1, month1, day1, daysInMonths) - calcDays(year2, month2, day2, daysInMonths));
+        return Math.abs(calcDays(date1) - calcDays(date2));
     }
 
-    private int calcDays(int year, int month, int day, int[] daysInMonths) {
+    private boolean isLeapYear(int year) {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    }
+
+    private int daysInMonth(int year, int month) {
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        days[1] += isLeapYear(year) ? 1 : 0;
+
+        return days[month - 1];
+    }
+
+    private int calcDays(String date) {
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8));
         int days = 0;
-        for (int y = 1971; y < year; y++) {
-            days += 365;
-            if (java.time.Year.of(y).isLeap()) {
-                days++;
-            }
+        for (int y = 1971; y < year; ++y) {
+            days += isLeapYear(y) ? 366 : 365;
         }
 
-        for (int m = 1; m < month; m++) {
-            days += daysInMonths[m - 1];
-            if (java.time.Year.of(year).isLeap()) {
-                days++;
-            }
+        for (int m = 1; m < month; ++m) {
+            days += daysInMonth(year, m);
         }
 
-        return days + day;
-    }
+        days += day;
 
-    private int year(String date) {
-        return Integer.parseInt(date.substring(0, 4));
-    }
-
-    private int month(String date) {
-        return Integer.parseInt(date.substring(5, 7));
-    }
-
-    private int day(String date) {
-        return Integer.parseInt(date.substring(8));
+        return days;
     }
 }
--- src/test/java/easy/_1300_1399/_1360/SolutionTest.java
+++ src/test/java/easy/_1300_1399/_1360/SolutionTest.java
@@ -12,5 +12,9 @@ public class SolutionTest {
         assertEquals(1, solution.daysBetweenDates("2019-06-29", "2019-06-30"));
 
         assertEquals(15, solution.daysBetweenDates("2020-01-15", "2019-12-31"));
+
+        assertEquals(14331, solution.daysBetweenDates("1971-06-29", "2010-09-23"));
+
+        assertEquals(25923, solution.daysBetweenDates("2009-08-18", "2080-08-08"));
     }
 }
