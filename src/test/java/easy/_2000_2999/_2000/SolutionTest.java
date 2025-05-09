24e51a76038203444033a386db4066171f851588
azastashkov <zastashkov@yahoo.com>
2025-05-09 19:15:44 +0300

    2000

---
+++ src/test/java/easy/_2000_2999/_2000/SolutionTest.java
@@ -0,0 +1,18 @@
+package easy._2000_2999._2000;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+
+        assertEquals("dcbaefd", solution.reversePrefix("abcdefd", 'd'));
+
+        assertEquals("zxyxxe", solution.reversePrefix("xyxzxe", 'z'));
+
+        assertEquals("abcd", solution.reversePrefix("abcd", 'z'));
+    }
+}
