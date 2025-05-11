fa8bfbab8eef6bc8da5971e223c2a5845e89f36f
azastashkov <zastashkov@yahoo.com>
2025-05-11 13:14:16 +0300

    2073

---
+++ src/test/java/easy/_2000_2999/_2073/SolutionTest.java
@@ -0,0 +1,16 @@
+package easy._2000_2999._2073;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+
+        assertEquals(6, solution.timeRequiredToBuy(new int[] { 2, 3, 2 }, 2));
+
+        assertEquals(8, solution.timeRequiredToBuy(new int[] { 5, 1, 1, 1 }, 0));
+    }
+}
