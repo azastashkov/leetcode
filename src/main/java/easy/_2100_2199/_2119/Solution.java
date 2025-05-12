278fd859d755d79fcf8610b49a617b525f21b367
azastashkov <zastashkov@yahoo.com>
2025-05-12 21:35:16 +0300

    2119

--- src/main/java/easy/_2100_2199/_2119/Solution.java
+++ src/main/java/easy/_2100_2199/_2119/Solution.java
@@ -2,11 +2,6 @@ package easy._2100_2199._2119;
 
 public class Solution {
     public boolean isSameAfterReversals(int num) {
-        StringBuilder sb = new StringBuilder();
-        int reversed1 = Integer.parseInt(sb.append(num).reverse().toString());
-        sb.setLength(0);
-        int reversed2 = Integer.parseInt(sb.append(reversed1).reverse().toString());
-
-        return num == reversed2;
+        return num == 0 || num % 10 != 0;
     }
 }
