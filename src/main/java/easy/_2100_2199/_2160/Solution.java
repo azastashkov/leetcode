f03864818361c33f70e80f225adba5a52782aba6
azastashkov <zastashkov@yahoo.com>
2025-05-18 00:42:42 +0300

    2160

---
+++ src/main/java/easy/_2100_2199/_2160/Solution.java
@@ -0,0 +1,17 @@
+package easy._2100_2199._2160;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int minimumSum(int num) {
+        int[] nums = new int[4];
+        for (int i = 0; num != 0; ++i) {
+            nums[i] = num % 10;
+            num /= 10;
+        }
+
+        Arrays.sort(nums);
+
+        return 10 * (nums[0] + nums[1]) + nums[2] + nums[3];
+    }
+}
---
+++ src/test/java/easy/_2100_2199/_2160/SolutionTest.java
@@ -0,0 +1,16 @@
+package easy._2100_2199._2160;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+
+        assertEquals(52, solution.minimumSum(2932));
+
+        assertEquals(13, solution.minimumSum(4009));
+    }
+}
