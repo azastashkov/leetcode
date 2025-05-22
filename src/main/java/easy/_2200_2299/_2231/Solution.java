1029d92e066f4f126781f7cd72870133fd6effc5
azastashkov <zastashkov@yahoo.com>
2025-05-22 22:09:13 +0300

    2231

--- src/main/java/easy/_2200_2299/_2231/Solution.java
+++ src/main/java/easy/_2200_2299/_2231/Solution.java
@@ -2,58 +2,21 @@ package easy._2200_2299._2231;
 
 public class Solution {
     public int largestInteger(int num) {
-        int[] digits = new int[32];
-        int i = 31;
-        while (num > 0) {
-            digits[i--] = num % 10;
-            num /= 10;
+        char[] s = String.valueOf(num).toCharArray();
+        int[] cnt = new int[10];
+        for (char c : s) {
+            ++cnt[c - '0'];
         }
-
-        int j = 31, k = i + 1;
-        while (k < j) {
-            if (digits[k] % 2 != 0) {
-                k++;
-            } else if (digits[j] % 2 != 0) {
-                j--;
-            } else {
-                if (digits[j] > digits[k]) {
-                    swap(digits, k, j);
-                    k++;
-                }
-                j--;
+        int[] idx = {8, 9};
+        int ans = 0;
+        for (char c : s) {
+            int x = c - '0';
+            while (cnt[idx[x & 1]] == 0) {
+                idx[x & 1] -= 2;
             }
+            ans = ans * 10 + idx[x & 1];
+            cnt[idx[x & 1]]--;
         }
-
-        k = i + 1;
-        j = 31;
-        while (k < j) {
-            if (digits[k] % 2 == 0) {
-                k++;
-            } else if (digits[j] % 2 == 0) {
-                j--;
-            } else {
-                if (digits[j] > digits[k]) {
-                    swap(digits, k, j);
-                    k++;
-                }
-                j--;
-            }
-        }
-
-        int largest = 0, mult = 1;
-        j = 31;
-        while (digits[j] > 0) {
-            largest += digits[j] * mult;
-            mult *= 10;
-            j--;
-        }
-
-        return largest;
-    }
-
-    private void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+        return ans;
     }
 }
--- src/test/java/easy/_2200_2299/_2231/SolutionTest.java
+++ src/test/java/easy/_2200_2299/_2231/SolutionTest.java
@@ -12,5 +12,7 @@ public class SolutionTest {
         assertEquals(3412, solution.largestInteger(1234));
 
         assertEquals(87655, solution.largestInteger(65875));
+
+        assertEquals(60, solution.largestInteger(60));
     }
 }
