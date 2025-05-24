a59199cabab298620190e49701a1ff8d438a5c6d
azastashkov <zastashkov@yahoo.com>
2025-05-24 20:22:34 +0300

    2243

---
+++ src/main/java/easy/_2200_2299/_2243/Solution.java
@@ -0,0 +1,22 @@
+package easy._2200_2299._2243;
+
+public class Solution {
+    public String digitSum(String s, int k) {
+        int l = s.length();
+        while (l > k) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < l; i += k) {
+                int sum = 0;
+                int end = Math.min(i + 3, l);
+                for (int j = i; j < end; j++) {
+                    sum += (s.charAt(j) - '0');
+                }
+                sb.append(sum);
+            }
+            s = sb.toString();
+            l = s.length();
+        }
+
+        return s;
+    }
+}
---
+++ src/test/java/easy/_2200_2299/_2243/SolutionTest.java
@@ -0,0 +1,16 @@
+package easy._2200_2299._2243;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+
+        assertEquals("135", solution.digitSum("11111222223", 3));
+
+        assertEquals("000", solution.digitSum("00000000", 3));
+    }
+}
