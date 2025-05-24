d28afcb2f371bf3797c4184532e1bd731b8e72a5
azastashkov <zastashkov@yahoo.com>
2025-05-24 19:57:09 +0300

    2239

--- src/main/java/easy/_2200_2299/_2239/Solution.java
+++ src/main/java/easy/_2200_2299/_2239/Solution.java
@@ -3,12 +3,16 @@ package easy._2200_2299._2239;
 public class Solution {
     public int findClosestNumber(int[] nums) {
         int minDistance = Integer.MAX_VALUE;
-        int closest = Integer.MAX_VALUE;
+        int closest = Integer.MIN_VALUE;
         for (int n : nums) {
             int d = Math.abs(n);
-            if (d <= minDistance) {
+            if (d < minDistance) {
                 minDistance = d;
                 closest = n;
+            } else if (d == minDistance) {
+                if (n > closest) {
+                    closest = n;
+                }
             }
         }
 
--- src/test/java/easy/_2200_2299/_2239/SolutionTest.java
+++ src/test/java/easy/_2200_2299/_2239/SolutionTest.java
@@ -12,5 +12,7 @@ public class SolutionTest {
         assertEquals(1, solution.findClosestNumber(new int[] { -4, -2, 1, 4, 8 }));
 
         assertEquals(1, solution.findClosestNumber(new int[] { 2, -1, 1 }));
+
+        assertEquals(1, solution.findClosestNumber(new int[] { 2, 1, 1, -1, 100000 }));
     }
 }
