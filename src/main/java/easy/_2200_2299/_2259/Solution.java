24d73da68786b47953faad68ddbe6eebaf39b41a
azastashkov <zastashkov@yahoo.com>
2025-05-24 23:11:37 +0300

    2259

--- src/main/java/easy/_2200_2299/_2259/Solution.java
+++ src/main/java/easy/_2200_2299/_2259/Solution.java
@@ -1,15 +1,20 @@
 package easy._2200_2299._2259;
 
+import java.math.BigDecimal;
+
 public class Solution {
     public String removeDigit(String number, char digit) {
-        int fromIndex = 0, splitIndex = 0, l = number.length(), max = Integer.MIN_VALUE;
+        int fromIndex = 0, splitIndex, l = number.length();
+        BigDecimal max = new BigDecimal(Integer.MIN_VALUE);
         while ((splitIndex = number.indexOf(digit, fromIndex)) >= 0) {
             fromIndex = splitIndex + 1;
             String s = number.substring(0, splitIndex) + number.substring(Math.min(fromIndex, l));
-            int n = Integer.parseInt(s);
-            max = Math.max(max, n);
+            BigDecimal n = new BigDecimal(s);
+            if (n.compareTo(max) > 0) {
+                max = n;
+            }
         }
 
-        return Integer.toString(max);
+        return max.toString();
     }
 }
