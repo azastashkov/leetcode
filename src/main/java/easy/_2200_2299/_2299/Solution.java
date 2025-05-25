30e66cfe45d5e6572f82d5d119a88f64ec5edc04
azastashkov <zastashkov@yahoo.com>
2025-05-25 14:32:41 +0300

    2299

--- src/main/java/easy/_2200_2299/_2299/Solution.java
+++ src/main/java/easy/_2200_2299/_2299/Solution.java
@@ -2,33 +2,25 @@ package easy._2200_2299._2299;
 
 public class Solution {
     public boolean strongPasswordCheckerII(String password) {
-        int l = password.length();
-        if (l < 8) {
+        if (password.length() < 8) {
             return false;
         }
-
-        if (!password.matches(".*[a-z].*")) {
-            return false;
-        }
-
-        if (!password.matches(".*[A-Z].*")) {
-            return false;
-        }
-
-        if (!password.matches(".*[0-9].*")) {
-            return false;
-        }
-
-        if (!password.matches(".*[!@#$%^&*()\\-+].*")) {
-            return false;
-        }
-
-        for (int i = 1; i < l; i++) {
-            if (password.charAt(i - 1) == password.charAt(i)) {
+        int mask = 0;
+        for (int i = 0; i < password.length(); ++i) {
+            char c = password.charAt(i);
+            if (i > 0 && c == password.charAt(i - 1)) {
                 return false;
             }
+            if (Character.isLowerCase(c)) {
+                mask |= 1;
+            } else if (Character.isUpperCase(c)) {
+                mask |= 2;
+            } else if (Character.isDigit(c)) {
+                mask |= 4;
+            } else {
+                mask |= 8;
+            }
         }
-
-        return true;
+        return mask == 15;
     }
 }
