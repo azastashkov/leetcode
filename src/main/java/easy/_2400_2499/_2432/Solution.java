ef78cae220c8eceefc433fd43b4917d911c5f6e1
azastashkov <zastashkov@yahoo.com>
2025-06-12 19:31:43 +0300

    2432

--- src/main/java/easy/_2400_2499/_2432/Solution.java
+++ src/main/java/easy/_2400_2499/_2432/Solution.java
@@ -1,42 +1,19 @@
 package easy._2400_2499._2432;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 public class Solution {
     public int hardestWorker(int n, int[][] logs) {
-        PriorityQueue<Employee> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        int lastTaskStartTime = 0;
-        int l = logs.length;
+        int ans = 0;
+        int last = 0, mx = 0;
         for (int[] log : logs) {
-            int id = log[0];
-            int leaveTime = log[1];
-            int timeSpent = leaveTime - lastTaskStartTime;
-            lastTaskStartTime = leaveTime;
-            pq.add(new Employee(id, timeSpent));
-        }
-
-        return pq.remove().id;
-    }
-
-    private static class Employee implements Comparable<Employee> {
-        int id;
-        int timeSpent;
-
-        public Employee(int id, int timeSpent) {
-            this.id = id;
-            this.timeSpent = timeSpent;
-        }
-
-        @Override
-        public int compareTo(Employee other) {
-            if (timeSpent > other.timeSpent) {
-                return 1;
-            } else if (timeSpent < other.timeSpent) {
-                return -1;
+            int uid = log[0], t = log[1];
+            t -= last;
+            if (mx < t || (mx == t && ans > uid)) {
+                ans = uid;
+                mx = t;
             }
-
-            return Integer.compare(other.id, id);
+            last += t;
         }
+
+        return ans;
     }
 }
