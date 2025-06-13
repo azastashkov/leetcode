21fb900a75fe1e5a4e2dc45f4ee2fc8c4b1ab1d6
azastashkov <zastashkov@yahoo.com>
2025-06-13 20:12:17 +0300

    2578

---
+++ src/test/java/easy/_2500_2599/_2578/SolutionTest.java
@@ -0,0 +1,16 @@
+package easy._2500_2599._2578;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+
+        assertEquals(59, solution.splitNum(4325));
+
+        assertEquals(75, solution.splitNum(687));
+    }
+}
