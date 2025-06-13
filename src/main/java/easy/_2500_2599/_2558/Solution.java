e0bf1dcc628e74f28ab49b68c2e2b03f1144a5ee
azastashkov <zastashkov@yahoo.com>
2025-06-13 19:02:18 +0300

    2558

--- src/main/java/easy/_2500_2599/_2558/Solution.java
+++ src/main/java/easy/_2500_2599/_2558/Solution.java
@@ -1,27 +1,20 @@
 package easy._2500_2599._2558;
 
-import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class Solution {
     public long pickGifts(int[] gifts, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int g : gifts) {
-            pq.add(g);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int v : gifts) {
+            pq.offer(v);
         }
-
-        int i = 0;
-        while (i < k) {
-            Integer max = pq.poll();
-            pq.add((int)Math.sqrt(max));
-            i++;
+        while (k-- > 0) {
+            pq.offer((int) Math.sqrt(pq.poll()));
         }
-
-        int sum = 0;
-        while (!pq.isEmpty()) {
-            sum += pq.poll();
+        long ans = 0;
+        for (int v : pq) {
+            ans += v;
         }
-
-        return sum;
+        return ans;
     }
 }
