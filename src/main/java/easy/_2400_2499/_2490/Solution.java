6a4c263bb937806a7720f036ebdd5e8995371806
azastashkov <zastashkov@yahoo.com>
2025-06-13 14:30:34 +0300

    2490

--- src/main/java/easy/_2400_2499/_2490/Solution.java
+++ src/main/java/easy/_2400_2499/_2490/Solution.java
@@ -4,18 +4,13 @@ public class Solution {
     public boolean isCircularSentence(String sentence) {
         String[] words = sentence.split("\\W+");
         int l = words.length;
-        if (l == 1) {
-            return sentence.charAt(0) == sentence.charAt(sentence.length() - 1);
-        }
 
-        for (int i = 1; i <= l - 1; i++) {
-            String prev = words[i - 1];
-            String curr = words[i];
-            if (prev.charAt(prev.length() - 1) != curr.charAt(0)) {
+        for (int i = 0; i < l; i++) {
+            if (words[i].charAt(words[i].length() - 1) != words[(i + 1) % l].charAt(0)) {
                 return false;
             }
         }
 
-        return words[0].charAt(0) == words[l - 1].charAt(words[l - 1].length() - 1);
+        return true;
     }
 }
