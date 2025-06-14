44b9434d44457a47ffce67b998598ac4a1b64336
azastashkov <zastashkov@yahoo.com>
2025-06-14 22:47:06 +0300

    2903

--- src/main/java/easy/_2900_2999/_2903/Solution.java
+++ src/main/java/easy/_2900_2999/_2903/Solution.java
@@ -2,15 +2,24 @@ package easy._2900_2999._2903;
 
 public class Solution {
     public int[] findIndices(int[] nums, int indexDifference, int valueDifference) {
-        int l = nums.length;
-        for (int i = 0; i < l; i++) {
-            for (int j = i + 1; j < l; j++) {
-                if (Math.abs(i - j) >= indexDifference && Math.abs(nums[i] - nums[j]) >= valueDifference) {
-                    return new int[] { i , j };
-                }
+        int mi = 0;
+        int mx = 0;
+        for (int i = indexDifference; i < nums.length; ++i) {
+            int j = i - indexDifference;
+            if (nums[j] < nums[mi]) {
+                mi = j;
+            }
+            if (nums[j] > nums[mx]) {
+                mx = j;
+            }
+            if (nums[i] - nums[mi] >= valueDifference) {
+                return new int[] {mi, i};
+            }
+            if (nums[mx] - nums[i] >= valueDifference) {
+                return new int[] {mx, i};
             }
         }
 
-        return new int[] { -1, -1 };
+        return new int[] {-1, -1};
     }
 }
