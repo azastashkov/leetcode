6866b0cd4b52458e55efb858422b828ed3edccee
azastashkov <zastashkov@yahoo.com>
2025-06-14 21:51:28 +0300

    2864

--- src/main/java/easy/_2800_2899/_2864/Solution.java
+++ src/main/java/easy/_2800_2899/_2864/Solution.java
@@ -2,16 +2,7 @@ package easy._2800_2899._2864;
 
 public class Solution {
     public String maximumOddBinaryNumber(String s) {
-        int l = s.length(), onesCount = 0;
-        for (int i = 0; i < l; i++) {
-            char c = s.charAt(i);
-            if (c == '1') {
-                onesCount++;
-            }
-        }
-
-        return "1".repeat(Math.max(0, onesCount - 1)) +
-                "0".repeat(Math.max(0, l - onesCount)) +
-                '1';
+        int cnt = s.length() - s.replace("1", "").length();
+        return "1".repeat(cnt - 1) + "0".repeat(s.length() - cnt) + "1";
     }
 }
