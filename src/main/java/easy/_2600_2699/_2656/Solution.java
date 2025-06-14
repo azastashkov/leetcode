5928f8e0365b2216b797bc8647cfe76ef677d29d
azastashkov <zastashkov@yahoo.com>
2025-06-14 14:10:19 +0300

    2656

--- src/main/java/easy/_2600_2699/_2656/Solution.java
+++ src/main/java/easy/_2600_2699/_2656/Solution.java
@@ -1,18 +1,12 @@
 package easy._2600_2699._2656;
 
-import java.util.Arrays;
-
 public class Solution {
     public int maximizeSum(int[] nums, int k) {
-        Arrays.sort(nums);
-
-        int l = nums.length, sum = 0;
-        while (k-- > 0) {
-            int t = nums[l - 1];
-            nums[l - 1] = t + 1;
-            sum += t;
+        int x = 0;
+        for (int v : nums) {
+            x = Math.max(x, v);
         }
 
-        return sum;
+        return k * x + k * (k - 1) / 2;
     }
 }
