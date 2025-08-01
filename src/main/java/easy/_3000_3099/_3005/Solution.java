49a9e4826e21a7563ebcf66220274b684731b1e9
azastashkov <zastashkov@yahoo.com>
2025-08-01 17:33:56 +0300

    3005

--- src/main/java/easy/_3000_3099/_3005/Solution.java
+++ src/main/java/easy/_3000_3099/_3005/Solution.java
@@ -2,18 +2,21 @@ package easy._3000_3099._3005;
 
 public class Solution {
     public int maxFrequencyElements(int[] nums) {
-        int[] frequencies = new int[101];
-        for (int n : nums) {
-            frequencies[n]++;
+        int[] cnt = new int[101];
+        for (int x : nums) {
+            ++cnt[x];
         }
 
-        int total = 0;
-        for (int f : frequencies) {
-            if (f > 0) {
-                total++;
+        int ans = 0, mx = -1;
+        for (int x : cnt) {
+            if (mx < x) {
+                mx = x;
+                ans = x;
+            } else if (mx == x) {
+                ans += x;
             }
         }
 
-        return total;
+        return ans;
     }
 }
--- src/test/java/easy/_3000_3099/_3005/SolutionTest.java
+++ src/test/java/easy/_3000_3099/_3005/SolutionTest.java
@@ -12,5 +12,7 @@ public class SolutionTest {
         assertEquals(4, solution.maxFrequencyElements(new int[] { 1, 2, 2, 3, 1, 4 }));
 
         assertEquals(5, solution.maxFrequencyElements(new int[] { 1, 2, 3, 4, 5 }));
+
+        assertEquals(2, solution.maxFrequencyElements(new int[] { 10, 12, 11, 9, 6, 19, 11 }));
     }
 }
