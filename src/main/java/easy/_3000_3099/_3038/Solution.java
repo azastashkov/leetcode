94f852a2fd33725f6dca852d1ec1ac5cf901e529
azastashkov <zastashkov@yahoo.com>
2025-08-06 16:30:57 +0300

    3038

---
+++ src/main/java/easy/_3000_3099/_3038/Solution.java
@@ -0,0 +1,13 @@
+package easy._3000_3099._3038;
+
+public class Solution {
+    public int maxOperations(int[] nums) {
+        int s = nums[0] + nums[1];
+        int ans = 0, n = nums.length;
+        for (int i = 0; i + 1 < n && nums[i] + nums[i + 1] == s; i += 2) {
+            ++ans;
+        }
+
+        return ans;
+    }
+}
---
+++ src/test/java/easy/_3000_3099/_3038/SolutionTest.java
@@ -0,0 +1,18 @@
+package easy._3000_3099._3038;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+
+        assertEquals(2, solution.maxOperations(new int[] { 3, 2, 1, 4, 5 }));
+
+        assertEquals(2, solution.maxOperations(new int[] { 1, 5, 3, 3, 4, 1, 3, 2, 2, 3 }));
+
+        assertEquals(1, solution.maxOperations(new int[] { 5, 3 }));
+    }
+}
