fde5daf2804df16fdaaec3abc76b2641b7e69d70
azastashkov <zastashkov@yahoo.com>
2025-08-07 13:18:54 +0300

    3065

--- src/main/java/easy/_3000_3099/_3065/Solution.java
+++ src/main/java/easy/_3000_3099/_3065/Solution.java
@@ -1,18 +1,14 @@
 package easy._3000_3099._3065;
 
-import java.util.Arrays;
-
 public class Solution {
     public int minOperations(int[] nums, int k) {
-        Arrays.sort(nums);
-
-        int l = nums.length;
-        for (int i = 0; i < l; i++) {
-            if (nums[i] >= k) {
-                return i;
+        int count = 0;
+        for (int n : nums) {
+            if (n < k) {
+                count++;
             }
         }
 
-        return l;
+        return count;
     }
 }
